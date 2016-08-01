eba657b295b141285a2df338681f8d91818b22ef
ksera1 <sarah.sareh@gmail.com>
2016-08-01 18:58:28 +0430

    used .append method

--- fizz-buzz.js
+++ fizz-buzz.js
@@ -1,13 +1,18 @@
-for (var i = 0; i <= 100; i++) {
-    if (i % 15 == 0) {
-        console.log("FizzBuzz")
+$(document).ready(function() {
 
-    } else if (i % 5 == 0 && i % 3 != 0) {
-        console.log("Buzz")
+// ; - very optional in Javascript in some places
+    for (var i = 0; i <= 100; i++) {
+        if (i % 15 == 0) {
+            $('.fizz-buzz').append("<p>FizzBuzz</p>")
 
-    } else if (i % 3 == 0 && i % 5 != 0) {
-        console.log("Fizz")
-    } else {
-        console.log(i)
-    }
-};
+        } else if (i % 5 == 0) {
+            $('.fizz-buzz').append("<p>Buzz</p>")
+
+        } else if (i % 3 == 0) {
+            $('.fizz-buzz').append("<p>Fizz</p>")
+        } else {
+            $('.fizz-buzz').append("<p>" + i + "</p>")
+        }
+    };
+
+})
